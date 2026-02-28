e06e5a10ebd500f13b632bfb7bb41b72a0824589
AlexvZyl <alexandervanzyl@protonmail.com>
2026-02-28 19:17:53 +0200

    ref: Remove allocation from books_view

--- src/components/books_view.rs
+++ src/components/books_view.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
 use ratatui::prelude::Stylize;
-use ratatui::widgets::{Block, BorderType, Borders, List, ListItem};
+use ratatui::widgets::{Block, BorderType, Borders};
 
 pub struct BooksView<'a> {
     pub books: &'a Vec<String>,
@@ -10,31 +10,35 @@ pub struct BooksView<'a> {
 
 impl<'a> Widget for BooksView<'a> {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let visible = area.height.saturating_sub(2) as usize;
+        let block = Block::default()
+            .borders(Borders::ALL)
+            .border_type(BorderType::Rounded)
+            .title(" Books ".yellow().bold());
 
-        let items: Vec<ListItem> = self
+        let inner = block.inner(area);
+        block.render(area, buf);
+
+        for (row, (i, b)) in self
             .books
             .iter()
             .enumerate()
             .skip(self.scrolled_offset)
-            .take(visible)
-            .map(|(i, b)| {
-                let content = format!("> {}", b);
-                if i == self.selected_book_index {
-                    ListItem::new(content).cyan().bold()
-                } else {
-                    ListItem::new(format!("  {}", b))
-                }
-            })
-            .collect(); // TODO: Don't want this collect.
-
-        List::new(items)
-            .block(
-                Block::default()
-                    .borders(Borders::ALL)
-                    .border_type(BorderType::Rounded)
-                    .title(" Books ".yellow().bold()),
-            )
-            .render(area, buf);
+            .take(inner.height as usize)
+            .enumerate()
+        {
+            let line_area = Rect {
+                y: inner.y + row as u16,
+                height: 1,
+                ..inner
+            };
+            if i == self.selected_book_index {
+                Line::from(format!("> {}", b))
+                    .cyan()
+                    .bold()
+                    .render(line_area, buf);
+            } else {
+                Line::from(format!("  {}", b)).render(line_area, buf);
+            }
+        }
     }
 }
